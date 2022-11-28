b53667647249675ffd0320fbd61acc4f470667d9
Matheus <matheuspd07@gmail.com>
2022-11-28 09:07:34 -0300

    Update

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -23,14 +23,9 @@ import { RouterLink, RouterView } from 'vue-router'
 </template>
 
 <style scoped>
-    header {
+header {
   line-height: 1.5;
   max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
 }
 
 nav {
@@ -64,10 +59,6 @@ nav a:first-of-type {
     display: flex;
     place-items: center;
     padding-right: calc(var(--header-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
   }
 
   header .wrapper {
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -80,3 +80,16 @@
             
         </section>
 </template>
+
+<style scoped>
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+}
+</style>
